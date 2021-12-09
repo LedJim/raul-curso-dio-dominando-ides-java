f5e110c30a29303ab8c9ce7038fe2dc0953bae62
LedJim <raulrojas.totvs@gmail.com>
2021-12-09 01:17:22 -0300

    first commit

---
+++ eclipse/PrimeiroProgramaJava/src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,51 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num1, num2;
+		
+		System.out.println("Digite el primer número: ");
+		num1 = scan.nextInt();
+		System.out.println("Digite el segundo número: ");
+		num2 = scan.nextInt();
+		
+		int soma = soma(num1,num2);
+		int resta = resta(num1,num2);
+		int mult = mult(num1,num2);
+		float div = div(num1,num2);
+		
+		System.out.println("suma " + soma);
+		System.out.println("resta " + resta);
+		System.out.println("mult " + mult);
+		System.out.println("div " + div);
+		
+		
+		
+	}
+
+	public static int soma(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public static int resta(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	
+	public static int mult(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	
+	public static float div(int num1, int num2) {
+		return num1 / num2;
+	}
+	
+	
+	
+}
